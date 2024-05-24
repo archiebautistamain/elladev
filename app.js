a115cc847716ad3bb4bacc797f15ada5e4745a26
archiebautistamain <103757008+archiebautistamain@users.noreply.github.com>
2024-05-24 12:49:22 +0800

    fixed navigation brand

--- app.js
+++ app.js
@@ -10,6 +10,27 @@ $(function(){
         $(this).find('.card').toggleClass('flipped');
 
  });
+
+ let windowSize = $(window).width();
+  if(windowSize < 992){
+    $("#navigation-collapse").removeClass("d-none");
+    $("#navigation-brand").addClass("d-none");
+  } else {
+    $("#navigation-collapse").addClass("d-none");
+    $("#navigation-brand").removeClass("d-none")
+  }
+
+  $(window).resize(function(){
+    windowSize = $(window).width();
+    if(windowSize < 992){
+        $("#navigation-collapse").removeClass("d-none");
+        $("#navigation-brand").addClass("d-none");
+    } else {
+        $("#navigation-collapse").addClass("d-none");
+        $("#navigation-brand").removeClass("d-none");
+    }
+});
+
  $('.slider-for').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
@@ -25,6 +46,8 @@ $(function(){
     focusOnSelect: true,
     autoplay: true
   });
+
+  
 });
 
 function removeActive(){
