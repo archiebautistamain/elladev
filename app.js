65589b46a925f7affdcaa692b918dddfc40a43a2
archiebautistamain <103757008+archiebautistamain@users.noreply.github.com>
2024-06-05 20:21:10 +0800

    added fadeup

--- app.js
+++ app.js
@@ -55,9 +55,36 @@ $(function(){
       autoplay: true
     });
   }
+
+  
   
  
+  function check_if_in_view() {
+    var window_height = $window.height();
+    var window_top_position = $window.scrollTop();
+    var window_bottom_position = (window_top_position + window_height);
   
+    $.each($animation_elements, function() {
+      var $element = $(this);
+      var element_height = $element.outerHeight();
+      var element_top_position = $element.offset().top;
+      var element_bottom_position = (element_top_position + element_height);
+  
+      //check to see if this current container is within viewport
+      if ((element_bottom_position >= window_top_position) &&
+        (element_top_position <= window_bottom_position)) {
+        $element.slideUp("slow", function(){})
+      }
+    });
+  }
+
+  var $animation_elements = $('.animated');
+  var $window = $(window);
+
+ 
+
+  $window.on('scroll resize', check_if_in_view);
+  $window.trigger('scroll');
  
 });
 
@@ -66,4 +93,5 @@ function removeActive(){
         $(this).removeClass("active");
     });
 }
+
 
